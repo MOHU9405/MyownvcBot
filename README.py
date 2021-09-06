46fd921cdcee1fcfaccaca5ac92a5fdc0a6ba414
iamdivyeshh <86907707+iamdivyeshh@users.noreply.github.com>
2021-09-06 12:18:47 +0530

    Create README.py

---
+++ README.py
@@ -0,0 +1,9 @@
+### Deploy To Heroku 📡</h4>
+
+<p align="left">
+
+  <a href="https://heroku.com/deploy?template=https://github.com/TG-Musics/Telegram_Vc_Bot">
+
+     <img height="30px" src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku">
+
+  </a>
